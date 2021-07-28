e7cb7a78ac39e48bc08d7feadb2b7772a6d5a1b1
iahmed5 <example.com>
2021-07-29 00:27:52 +0100

    Initial commit

---
+++ src/main/java/com/iahmed5/springreactwebapp/SpringReactWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.iahmed5.springreactwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iahmed5/springreactwebapp/SpringReactWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iahmed5.springreactwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
